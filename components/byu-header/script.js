8a3d578162e1c0b8398348fb82ff956c316c46e5
katrialesser <katrialynn18@gmail.com>
2017-02-14 12:18:03 -0700

    moved my script for adding login info into menus into the user-login component, adjusted id names. added svg image

--- components/byu-header/script.js
+++ components/byu-header/script.js
@@ -9,62 +9,6 @@
             shadowRoot.innerHTML = template;
         }
         connectedCallback() {
-            // get attribute value from byu-header
-            //const loggedInStatus =  get observedAttributes() {return ['loggedin']; }
-            const header = this.shadowRoot.querySelector("#byu-header");
-            var loggedIn = header.getAttribute(loggedin);
-            if (loggedIn) {
-                //signIn.style.cueBefore = "byu-user-login/images/user-sign-in.svg";
-                const signOut = this.shadowRoot.querySelector("#sign-out");
-                var signOutLink = signOut.assignedNodes().filter(function (element) {
-                    return element instanceof HTMLElement
-                });
-                signOutLink.style.float = "right";
-                signOutLink.style.marginRight = "10px";
-                const username = this.shadowRoot.querySelector("#username");
-                var usernameLink = username.assignedNodes().filter(function (element) {
-                    return element instanceof HTMLElement
-                });
-                usernameLink.style.color = "#003DA5";
-
-            } else {
-                // get sign in
-                const signIn = this.shadowRoot.querySelector("#sign-in");
-                var signInLink = signIn.assignedNodes().filter(function (element) {
-                    return element instanceof HTMLElement
-                });
-            }
-            // create First Row section with icon, my account, and sign out links
-            var firstRow = document.createElement("div");
-            firstRow.style.display = "flex";
-            firstRow.style.justifyContent = "space-between";
-
-            // top left
-            //var firstRowLeft = document.createElement("div");
-            //firstRowLeft.style.display = "flex";
-            //firstRowLeft.style.justifyContent = "flex-start";
-
-            if(loggedIn) {
-                // show account & sign out
-                var icon = document.createElement("img");
-                icon.source("byu-user-logout/images/user-sign-out.svg");
-                firstRow.appendChild(icon);
-                firstRow.appendChild(usernameLink);
-                //firstRow.appendChild(firstRowLeft);
-                firstRow.appendChild(signOutLink);
-            } else {
-                //show sign in...
-                var icon = document.createElement("img");
-                icon.source("byu-user-logout/images/user-sign-out.svg");
-                firstRow.appendChild(icon);
-                firstRow.appendChild(signInLink);
-                //firstRow.appendChild(firstRowLeft);
-            }
-
-            // prepend this FirstRow to menu links
-            //Example:  section.insertBefore( span, section.firstChild );
-            this.shadowRoot.querySelector('#navbarMenu').assignedNodes[0].shadowRoot.querySelector('secondary-nav').insertBefore( firstRow, this.shadowRoot.querySelector('#navbarMenu').assignedNodes[0].shadowRoot.querySelector('secondary-nav').firstChild );
-
 
             // add Action Links to dropdown menu
             const actions = this.shadowRoot.querySelector("#action-links");
--- components/byu-user-login/script.js
+++ components/byu-user-login/script.js
@@ -25,6 +25,45 @@
             return this.getAttribute('login-url');
         }
 
+        set logoutUrl(value) {
+            let link = this.shadowRoot.querySelector('.link');
+
+            if (value) {
+                this.setAttribute('logout-url', value);
+                if (link) {
+                    link.setAttribute('href', value);
+                }
+            } else {
+                this.removeAttribute('logout-url');
+                if (link) {
+                    link.removeAttribute('href');
+                }
+            }
+        }
+
+        get logoutUrl() {
+            return this.getAttribute('login-url');
+        }
+        set myaccountUrl(value) {
+            let link = this.shadowRoot.querySelector('.link');
+
+            if (value) {
+                this.setAttribute('myaccount-url', value);
+                if (link) {
+                    link.setAttribute('href', value);
+                }
+            } else {
+                this.removeAttribute('myaccount-url');
+                if (link) {
+                    link.removeAttribute('href');
+                }
+            }
+        }
+
+        get myaccountUrl() {
+            return this.getAttribute('myaccount-url');
+        }
+
         constructor() {
             super();
 
@@ -32,6 +71,8 @@
             shadowRoot.innerHTML = template;
             //Hack to make sure that the proper login URL gets set in our template.
             this.loginUrl = this.loginUrl;
+            this.myaccountUrl = this.myaccountUrl;
+            this.logoutUrl = this.logoutUrl;
         }
 
         static get observedAttributes() {
@@ -43,12 +84,79 @@
                 case 'login-url':
                     this.loginUrl = newval;
                     break;
+                case 'myaccount-url':
+                    this.myaccountUrl = newval;
+                    break;
+                case 'logout-url':
+                    this.logoutUrl = newval;
+                    break;
             }
         }
 
         connectedCallback() {
             this._addSlotListeners();
             this._addAriaAttributes();
+
+            // get attribute value from byu-header
+            //const loggedInStatus =  get observedAttributes() {return ['loggedin']; }
+            const header = this.shadowRoot.querySelector("#byu-header");
+            //var loggedIn = header.getAttribute(loggedin);
+
+            const username = this.shadowRoot.querySelector("#username");
+            var usernameLink = username.assignedNodes().filter(function (element) {
+                return element instanceof HTMLElement
+            });
+            usernameLink.style.color = "#003DA5";
+
+            if(usernameLink !== null) {
+                var loggedIn = true;
+            } else {
+                var loggedIn = false;
+            }
+
+            if (loggedIn) {
+                //signIn.style.cueBefore = "byu-user-login/images/user-sign-in.svg";
+                const logOut = this.shadowRoot.querySelector("#logout");
+                var logOutLink = logOut.assignedNodes().filter(function (element) {
+                    return element instanceof HTMLElement
+                });
+                logOutLink.style.float = "right";
+                logOutLink.style.marginRight = "10px";
+
+            } else {
+                // get sign in
+                const signIn = this.shadowRoot.querySelector("#login");
+                var signInLink = signIn.assignedNodes().filter(function (element) {
+                    return element instanceof HTMLElement
+                });
+            }
+            // create First Row section with icon, my account, and sign out links
+            var firstRow = document.createElement("div");
+            firstRow.style.display = "flex";
+            firstRow.style.justifyContent = "space-between";
+
+
+
+            if(loggedIn) {
+                // show account & sign out
+                var icon = document.createElement("img");
+                icon.source("byu-user-logout/images/user-sign-out.svg");
+                firstRow.appendChild(icon);
+                firstRow.appendChild(usernameLink);
+                firstRow.appendChild(logOutLink);
+            } else {
+                //show sign in...
+                var icon = document.createElement("img");
+                icon.source("byu-user-logout/images/user-sign-out.svg");
+                firstRow.appendChild(icon);
+                firstRow.appendChild(signInLink);
+            }
+
+            // prepend this FirstRow to menu links
+            //Example:  section.insertBefore( span, section.firstChild );
+            this.shadowRoot.querySelector('#navbarMenu').assignedNodes[0].shadowRoot.querySelector('secondary-nav').insertBefore( firstRow, this.shadowRoot.querySelector('#navbarMenu').assignedNodes[0].shadowRoot.querySelector('secondary-nav').firstChild );
+
+
         }
 
         _addSlotListeners() {
@@ -73,6 +181,8 @@
             if (link.href) {
                 console.log('setting login url from', link);
                 this.loginUrl = link.href;
+                this.myaccountUrl = link.href;
+                this.logoutUrl = link.href;
             }
         }
 
